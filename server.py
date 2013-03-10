c542c4e577f7835c4d3a9fe3d99f028dbae59de6
captconrado <ottey001@gmail.com>
2013-03-09 17:57:57 -0800

    now produces random server address

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 import sys
 import BaseHTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
+from random import randint
 
+port_mix = randint(1000,9999)
 
 HandlerClass = SimpleHTTPRequestHandler
 ServerClass  = BaseHTTPServer.HTTPServer
@@ -10,7 +12,7 @@ Protocol     = "HTTP/1.0"
 if sys.argv[1:]:
     port = int(sys.argv[1])
 else:
-    port = 8000
+    port = port_mix
 server_address = ('127.0.0.1', port)
 
 HandlerClass.protocol_version = Protocol
@@ -19,3 +21,4 @@ httpd = ServerClass(server_address, HandlerClass)
 sa = httpd.socket.getsockname()
 print "Ready to role, Lets Go! servin' up your server at ", sa[0], "port", sa[1], "..." 
 httpd.serve_forever()
+
